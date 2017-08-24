2b01f5e99bb21a28cc3db00e290c4d72a7c11802
Maymity <amatori.andrea@libero.it>
2017-08-24 12:57:26 +0200

    Small performance Update

--- src/it/maymity/freezegui/Main.java
+++ src/it/maymity/freezegui/Main.java
@@ -14,7 +14,7 @@ import java.util.logging.Logger;
 public class Main extends JavaPlugin implements Listener {
 
     Logger log;
-    public static double config_version = 1.2;
+    public static double config_version = 1.3;
 
     public void onEnable() {
         System.out.println("FreezeGUI > Start plugin...");
---
+++ src/it/maymity/freezegui/managers/MessagesManager.java
@@ -0,0 +1,33 @@
+package it.maymity.freezegui.managers;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import it.maymity.freezegui.Utils;
+
+public class MessagesManager
+{
+    private static MessagesManager instance;
+
+    public static synchronized MessagesManager getInstance()
+    {
+        if (instance == null) {
+            instance = new MessagesManager();
+        }
+        return instance;
+    }
+
+    String prefix = Utils.getInstance().getConfig().getString("settings.prefix");
+
+    public void sendMessage(Player p, String s)
+    {
+        p.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + s));
+    }
+
+    public void sendMessage(CommandSender p, String s)
+    {
+        p.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + s));
+    }
+
+}
+
